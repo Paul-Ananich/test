e8f8ffc0afc4ae5fe6ae07bf4053c16e93a3e5b8
Paul-Ananich <ananich.pavel@gmail.com>
2020-06-19 17:29:30 +0300

    'Added Backend'

--- App.js
+++ App.js
@@ -1,19 +1,130 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useCallback} from 'react';
+import {TouchableOpacity, Linking, ActivityIndicator, FlatList, StyleSheet, Text, View} from 'react-native';
+import {Button} from 'native-base';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+    let [data, setData] = useState([0])
+    let [isFetched, setIsFetched] = useState(false)
+    let [isLoading, setIsLoading] = useState(false)
+
+    const fetchData = async () => {
+        fetch('http://192.168.0.118:5000/message')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                return (
+                    setData(responseJson),
+                        setIsFetched(true),
+                        setIsLoading(false)
+                );
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    const showData = () => {
+        return (
+            setIsLoading(true),
+                fetchData()
+        )
+    }
+
+    const renderFlatListItem = (item) => {
+        return item ?
+            <View style={styles.content}>
+                <Text style={styles.name}>{item.name}</Text>
+                <OpenURLButton url={item.source_url}
+                children={new Date(item.date).toLocaleDateString('en-US')}/>
+                <Text style={styles.substance}>{item.essence}</Text>
+            </View>
+            : null
+    };
+
+    const OpenURLButton = ({ url, children }) => {
+        const handlePress = useCallback(async () => {
+            const supported = await Linking.canOpenURL(url);
+            if (supported) {
+                await Linking.openURL(url);
+            } else {
+                Alert.alert(`Don't know how to open this URL: ${url}`);
+            }
+        }, [url]);
+        return (
+            <Button style={styles.dateButton} onPress={handlePress}>
+                <Text>{children}</Text>
+            </Button>
+        )
+    }
+
+    const but = () => {
+        return (
+            isLoading ?
+                <ActivityIndicator size="large" color="#e91e63"/> :
+                <Button style={styles.button}
+                        onPress={() => showData()}
+                        full rounded success>
+                    <Text style={styles.txt}>Istagram сегодня</Text>
+                </Button>
+        )
+    }
+
+    return (
+        <View style={styles.container}>
+            {isFetched ?
+                <View style={styles.contView}>
+                    <FlatList
+                        data={data}
+                        keyExtractor={item => item.essence}
+                        renderItem={({item}) => renderFlatListItem(item)}
+                    />
+                </View>
+                :
+                but()
+            }
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+    contView: {
+        marginTop: 25,
+        flex: 1
+    },
+    button: {
+        backgroundColor: '#e91e63',
+        margin: 60,
+        height: 60
+    },
+    txt: {
+        color: 'white',
+        fontSize: 25,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    content: {
+        flex: 1,
+        margin: 10,
+    },
+    name: {
+        fontWeight: 'bold',
+        fontSize: 20
+    },
+    date: {
+        fontSize: 15,
+        color: '#464646'
+    },
+    substanceText: {
+        fontSize: 15
+    },
+    dateButton: {
+        marginRight: '70%',
+        backgroundColor: 'transparent'
+    }
 });
---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const host = '5.9.55.116';
+const port = '5000';
+const mysql = require('mysql');
+
+app.get('/message',  function(request, response) {
+    const connection = mysql.createConnection({
+        host: host,
+        user: 'test',
+        password: 'whigdyetiabEnok8',
+        database: 'test'
+    });
+    module.exports = connection;
+    connection.connect(function(err) {
+        if (err) throw err;
+        connection.query('SELECT message.date, message.source_url, account.name, essence.essence\n' +
+            'FROM user\n' +
+            'INNER JOIN user_subscription ON user_subscription.user_id = user.id\n' +
+            'INNER JOIN account ON account.id = user_subscription.account_id\n' +
+            'INNER JOIN message on message.account_id = user_subscription.account_id\n' +
+            'INNER JOIN essence on essence.message_id = message.id\n' +
+            'WHERE user.id = 1',
+            function (error, results, fields) {
+                // If some error occurs, we throw an error.
+                if (error) console.log(error);
+
+                // Getting the 'response' from the database and sending it to our route. This is were the data is.
+                console.log(results, 'results')
+                response.send(results)
+            });
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}/message`));
